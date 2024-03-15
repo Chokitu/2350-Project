25453c5b0ce4f6f9b197196dafd18bb9d3389d73
Alex Mendez <rmendezmartine00@mylangara.ca>
2024-03-14 23:40:24 -0700

    added logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -29,8 +29,8 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
-            <i class='fab fa-typo3' />
+            AiLearnt
+            <img src="/images/ailearnt-logo.png" alt="Logo" className="navbar-logo-image" />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
