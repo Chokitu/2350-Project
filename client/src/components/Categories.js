bf74b426c373f6227e15b50e794160cd566f55d0
Alex Mendez <rmendezmartine00@mylangara.ca>
2024-03-19 22:01:14 -0700

    space changes to cards

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   Typography,
   AppBar,
@@ -11,6 +12,7 @@ import {
   Toolbar,
   Container,
   Button,
+  Grow,
 } from "@mui/material";
 
 import { useTheme } from "@mui/material/styles";
@@ -37,40 +39,46 @@ const cards = [
   { imageName: "impact.png", heading: "Impact" },
 ];
 
+
+
 const MyGrid = styled(Grid)`
 
 `;
 
 function Categories() {
-  const theme = useTheme();
-  return (
-    <>
-      <MyContainer maxWidth="xl">
-        <MyGrid container spacing={4}>
-          {cards.map((card, index) => (
-            <Grid item key={index} xs={12} sm={6} md={4}>
-              <MyCard>
-                <MyCardContent>
-                <MyTypography variant="h5" gutterBottom align="center">
-                    {card.heading}
-                </MyTypography>
-                </MyCardContent>
-                <MyCardMedia
-                  image={`/images/${card.imageName}`}
-                  title="Image title"
-                />
-                <CardActions>
-                  <Button size="small" color="primary">
-                    View
-                  </Button>
-                </CardActions>
-              </MyCard>
-            </Grid>
-          ))}
-        </MyGrid>
-      </MyContainer>
-    </>
-  );
-}
-
-export default Categories;
+    const theme = useTheme();
+    return (
+      <>
+        <MyContainer maxWidth="xl">
+          <MyGrid container spacing={4}>
+            {cards.map((card, index) => (
+              <Grid item key={index} xs={12} sm={6} md={4}>
+                <Grow in timeout={(index || 0) * 500}>
+                  <Link to={`/${card.heading.toLowerCase()}`} style={{ textDecoration: 'none' }}>
+                    <MyCard>
+                      <MyCardContent>
+                        <MyTypography variant="h5" gutterBottom align="center">
+                          {card.heading}
+                        </MyTypography>
+                      </MyCardContent>
+                      <MyCardMedia
+                        image={`/images/${card.imageName}`}
+                        title="Image title"
+                      />
+                      <CardActions>
+                        <Button size="small" color="primary">
+                          &nbsp;
+                        </Button>
+                      </CardActions>
+                    </MyCard>
+                  </Link>
+                </Grow>
+              </Grid>
+            ))}
+          </MyGrid>
+        </MyContainer>
+      </>
+    );
+  }
+  
+  export default Categories;
